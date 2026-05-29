cf64ebe991c3cf19519ecd3e0d9727267cb48b54
Shraddha Agarwal <shraddha.agarwal1803@email.com>
2026-05-29 17:45:19 +0530

    Completed backend setup and authentication system

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,8 @@ mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch((err) => console.log(err))
 
+app.use("/api/auth", require("./routes/authRoutes"))
+
 app.get("/", (req, res) => {
   res.send("API Running")
 })
@@ -21,5 +23,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
