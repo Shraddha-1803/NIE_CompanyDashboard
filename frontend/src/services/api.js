54d5cb6e1fce63238504801ee61b4f933b53e8a1
Shraddha Agarwal <shraddha.agarwal1803@email.com>
2026-06-17 20:28:45 +0530

    Fixed production API route

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const API = axios.create({
-  baseURL: "https://nie-companydashboard.onrender.com",
+  baseURL: "https://nie-companydashboard.onrender.com/api",
 });
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
